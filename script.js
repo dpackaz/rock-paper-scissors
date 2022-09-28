397e243e5890097110a388d49f3cd2524f5ba18b
Daniel Packer <danielapacker@gmail.com>
2022-09-28 12:07:19 -0700

    add pseudocode, index, gitignore, and README

---
+++ script.js
@@ -0,0 +1,22 @@
+// Wireframe
+    // Basic HTML
+
+// Results
+    // Input Rock, beats scissors, loses to paper
+    // Input Paper, beats rock, loses to paper
+    // Input Scissors, beats paper, loses to rock
+// Tracker
+    // Track wins, losses, and ties
+
+//Process
+    //Prompt user to "Type R, S, or P"
+        //If other entry, "not a valid entry"
+        //Prompt again
+        //If user clicks "Cancel" quit program
+        //If user clicks "Send/Ok" move on
+    //Send user to computer-generated answer
+    //Send user to results tracker
+        //Display wins, losses, ties
+        //Prompt user:
+            //Play again?
+            //If yes, start again, if no exit program
